2f34ec04e94b268cc6606243406f7dbe7856a6a5
Denis Toledo <contato@denistoledo.com.br>
2018-09-06 17:06:40 -0300

    keep going

--- src/ui/components/my-component/component.js
+++ src/ui/components/my-component/component.js
@@ -1,5 +1,19 @@
 import Component from '@ember/component';
+import {
+  action,
+  computed,
+} from '@ember-decorators/object';
 
-export default Component.extend({
+export default class MyComponent extends Component {
+  count = 0;
 
-});
+  @computed('name')
+  get uppercaseName() {
+    return this.name.toUpperCase();
+  }
+
+  @action
+  increment() {
+    this.set('count', this.count + 1);
+  }
+};
